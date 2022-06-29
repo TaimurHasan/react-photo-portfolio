deae60043625691153aaf1c26157c92a2ad81372
TaimurHasan <taimurhasan11@gmail.com>
2022-06-28 22:30:11 -0400

    added tests for gallery and photolist

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         currentCategory = {currentCategory}
       />
       <main>
-          <Gallery />
+          <Gallery currentCategory={currentCategory}/>
           <About />        
       </main>
     </div>
