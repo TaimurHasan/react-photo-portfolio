8067ea4690add35614e0ca04962393cf2a3c38be
TaimurHasan <taimurhasan11@gmail.com>
2022-06-29 18:39:18 -0400

    added tests for contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -39,7 +39,7 @@ function ConcactForm() {
 
     return(
         <section>
-            <h1>Contact me</h1>
+            <h1 data-testid = "h1tag">Contact me</h1>
             <form id = "contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
